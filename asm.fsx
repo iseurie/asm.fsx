103e4243e701cbb86aa773f0be919ad24ec7f303
алый <iseurie@gmail.com>
2018-06-04 13:32:46 -0400

    Add a regular expression filter for FSharp.Core

--- asm.fsx
+++ asm.fsx
@@ -1,10 +1,13 @@
 open System
+open System.Text.RegularExpressions
 open System.IO open System.Diagnostics
+open System.Text.RegularExpressions
 let searchPath = Environment.ExpandEnvironmentVariables "%USERPROFILE%\\nuget"
 let scripts = Directory.EnumerateFiles(searchPath, "*.fsx", SearchOption.AllDirectories)
 let asm =
     Directory.EnumerateDirectories(searchPath, "*lib*", SearchOption.AllDirectories)
     |> Seq.collect (fun d -> Directory.EnumerateFiles(d, "*.dll", SearchOption.AllDirectories))
+    |> Seq.filter (Regex("FSharp.Core.dll").IsMatch>>not)
 
 let args =
     seq [
